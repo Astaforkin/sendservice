b2eaf1d93779a8c9f76099a606618c7adbde0ab1
Nikita Astaforkin <astaforkin90@gmail.com>
2024-01-27 22:08:48 +0300

    add fix and new file

--- mailing/models.py
+++ mailing/models.py
@@ -2,20 +2,44 @@ from django.db import models
 
 
 class Mailing(models.Model):
-    date_time_start_mailing = models.DateTimeField()
-    text_message = models.TextField()
-    mobile_code = models.CharField()
-    tag = models.CharField()
-    date_time_stop_mailing = models.DateTimeField()
+    date_time_start_mailing = models.DateTimeField(
+        verbose_name='дата и время запуска рассылки'
+    )
+    text_message = models.TextField(
+        max_length=255,
+        verbose_name='текст сообщения'
+    )
+    mobile_code = models.CharField(
+        max_length=100,
+        verbose_name='код мобильного оператора'
+    )
+    tag = models.CharField(max_length=100, verbose_name='тег')
+    date_time_stop_mailing = models.DateTimeField(
+        verbose_name='дата и время окончания рассылки'
+    )
 
 
 class Client(models.Model):
-    phone_number = models.CharField()
-    mobile_code = models.CharField()
-    tag = models.CharField()
+    phone_number = models.CharField(
+        max_length=11,
+        verbose_name='номер телефона клиента'
+    )
+    mobile_code = models.CharField(
+        max_length=3,
+        verbose_name='код мобильного оператора'
+    )
+    tag = models.CharField(max_length=100, verbose_name='тег')
 
 
 class Message(models.Model):
     date_time_send = models.DateTimeField()
-    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    mailing = models.ForeignKey(
+        Mailing,
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
+    client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Mailing, Client, Message
+from .serializers import MailingSerializer, ClientSerializer, MessageSerializer
+
+
+class MailingViewSet(viewsets.ModelViewSet):
+    queryset = Mailing.objects.all()
+    serializer_class = MailingSerializer
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer

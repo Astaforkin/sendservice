21910fe2c16c0214981d70cf2b0d61f9cdf3e7e6
Nikita Astaforkin <astaforkin90@gmail.com>
2024-01-27 18:36:58 +0300

    fix mailing/models.py

--- mailing/models.py
+++ mailing/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 
 class Mailing(models.Model):
-    pass
+    date_time_start_mailing = models.DateTimeField()
+    text_message = models.TextField()
+    mobile_code = models.CharField()
+    tag = models.CharField()
+    date_time_stop_mailing = models.DateTimeField()
 
 
 class Client(models.Model):
-    pass
+    phone_number = models.CharField()
+    mobile_code = models.CharField()
+    tag = models.CharField()
 
 
-class Messadge(models.Model):
-    pass
+class Message(models.Model):
+    date_time_send = models.DateTimeField()
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
--- sendservice/wsgi.py
+++ sendservice/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sendservice.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sendservice.settings")
 
 application = get_wsgi_application()

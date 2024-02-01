7476fb70facba80a83fe9c352592dd39d9075214
Nikita Astaforkin <astaforkin90@gmail.com>
2024-02-02 00:04:19 +0300

    add fix tasks.py

--- mailing/tasks.py
+++ mailing/tasks.py
@@ -4,35 +4,52 @@ import datetime
 import pytz
 
 
+# @app.task
+# def get_active_mailings():
+#     active_mailings = Mailing.objects.filter()
+#     for mailing in active_mailings:
+#         get_clients_for_mailing.delay(mailing)
+
+
+# @app.task
+# def get_clients_for_mailing(mailing):
+#     clients_for_mailing = Client.objects.filter(
+#         mobile_code=mailing.mobile_code,
+#         tag=mailing.tag
+#     )
+#     get_clients_no_message.delay(clients_for_mailing, mailing)
+
+
+# @app.task
+# def get_clients_no_message(clients, mailing):
+#     for client in clients:
+#         if not Message.objects.filter(mailing=mailing, client=client).exists():
+#             send_message_to_client.delay(client, mailing)
 @app.task
-def get_active_mailings():
-    active_mailings = Mailing.objects.filter(is_sent=False)
-    for mailing in active_mailings:
-        get_clients_for_mailing.delay(mailing)
-
-
-@app.task
-def get_clients_for_mailing(mailing):
-    clients_for_mailing = Client.objects.filter(
-        mobile_code=mailing.mobile_code,
-        tag=mailing.tag
-    )
-    get_clients_no_message.delay(clients_for_mailing, mailing)
-
-
-@app.task
-def get_clients_no_message(clients, mailing):
-    for client in clients:
-        if not Message.objects.filter(mailing=mailing, client=client, s_delivered=False).exists():
-            send_message_to_client.delay(client, mailing)
-
-
-@app.task
-def send_message_to_client(client_id, mailing_id):
-    client = Client.objects.get(pk=client_id)
-    mailing = Mailing.objects.get(pk=mailing_id)
-    timezone = pytz.timezone(client.timezone)
+def process_mailings():
+    mailings = Mailing.objects.all()
+    clients = Client.objects.all()
+    timezone = pytz.timezone(clients.timezone)
     now = datetime.now(timezone)
+    if mailings.date_time_start_mailing <= now.time() <= mailings.date_time_stop_mailing:
+        for mailing in mailings:
+            clients_for_mailing = Client.objects.filter(
+                mobile_code=mailing.mobile_code,
+                tag=mailing.tag
+            )
+            for client in clients_for_mailing:
+                if not Message.objects.filter(
+                    mailing=mailing, client=client
+                ).exists():
+                    send_message_to_client.delay(client, mailing)
 
-    if mailing.date_time_start_mailing <= now.time() <= mailing.date_time_stop_mailing:
-        
+
+@app.task
+def send_message_to_client(client, mailing):
+    timezone = pytz.timezone(client.timezone)
+    message = Message.objects.create(
+        pk=mailing,
+        pk=client,
+        date_time_send=timezone.now()
+    )
+    print(f"Сообщение отправлено клиенту {client} с текстом: {message}")

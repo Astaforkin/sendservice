ecd673f9ce48ecdfb8aab6f8878e754e8f88f83b
Nikita Astaforkin <astaforkin90@gmail.com>
2024-02-02 00:14:55 +0300

    add fix tasks.py

--- mailing/tasks.py
+++ mailing/tasks.py
@@ -1,6 +1,6 @@
 from .models import Mailing, Message, Client
 from sendservice.celery import app
-import datetime
+from django.utils import timezone
 import pytz
 
 
@@ -27,21 +27,21 @@ import pytz
 #             send_message_to_client.delay(client, mailing)
 @app.task
 def process_mailings():
-    mailings = Mailing.objects.all()
-    clients = Client.objects.all()
-    timezone = pytz.timezone(clients.timezone)
-    now = datetime.now(timezone)
-    if mailings.date_time_start_mailing <= now.time() <= mailings.date_time_stop_mailing:
-        for mailing in mailings:
-            clients_for_mailing = Client.objects.filter(
-                mobile_code=mailing.mobile_code,
-                tag=mailing.tag
-            )
-            for client in clients_for_mailing:
-                if not Message.objects.filter(
-                    mailing=mailing, client=client
+    now = timezone.now()
+    mailings = Mailing.objects.filter(
+        date_time_start_mailing__lte=now,
+        date_time_stop_mailing__gte=now
+    )
+    for mailing in mailings:
+        clients_for_mailing = Client.objects.filter(
+            mobile_code=mailing.mobile_code,
+            tag=mailing.tag
+        )
+        for client in clients_for_mailing:
+            if not Message.objects.filter(
+                mailing=mailing, client=client
                 ).exists():
-                    send_message_to_client.delay(client, mailing)
+                send_message_to_client.delay(client, mailing)
 
 
 @app.task
